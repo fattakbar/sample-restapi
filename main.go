620c51454350d6c76b8f8dbd33c63b703ba432eb
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-06-05 23:43:35 +0700

    Membuat Method GET, POST, PUT, DELETE

--- main.go
+++ main.go
@@ -1,9 +1,52 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+type Book struct {
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
+}
+
+type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func createBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func main() {
-	fmt.Println("Hello World")
+	r := mux.NewRouter()
+
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", createBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
